1932da5d507e4a52aec071cee0247240eb4b9cc5
unknown <muthonisophie12@gmail.com>
2025-07-21 15:59:07 +0300

    Simple Research AI

---
+++ main.py
@@ -0,0 +1,86 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+import langchain_openai
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+
+load_dotenv() # Load environment variables from .env file
+
+#set up an LLM
+# llm = ChatOpenAI(model="gpt-4o-mini")
+# llm = ChatGroq(
+#     model="llama3-70b-8192",  # or mixtral-8x7b-32768, gemma-7b-it
+# )
+
+# response = llm.invoke("What is the meaning of life?")
+# print(response.content)
+
+
+#Define a simple python class which will specify the type of cntent tahat we want our LLM to generate
+
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+llm = ChatGroq(
+    model="llama3-70b-8192",  # or mixtral-8x7b-32768, gemma-7b-it
+)
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse) 
+
+
+#Setting up a prompt template
+
+prompt = ChatPromptTemplate.from_messages(
+    [   
+        ("system", 
+         ''' 
+         You are a research assistant that will help generate a research paper
+         Answer the user query and use necssary tools
+         Wrap the output in this format and provide no other text \n {format_instructions}
+         '''
+         
+         ),
+         ("placeholder", "{chat_history }"),
+         ("human","{query}"),
+         ("placeholder", "{agent_scratchpad}"),
+        
+    ]
+
+).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_tool, wiki_tool, save_tool]
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt = prompt,
+    tools = tools
+
+)
+
+agent_executor = AgentExecutor(
+    agent=agent, tools=tools,
+    verbose=True,   )
+
+query = input("What do you want to research? ")
+raw_response = agent_executor.invoke({"query": query})
+
+
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+
+except Exception as e:
+    print(f"Error parsing response: {e}")
+    print("Raw response:", raw_response)
+
+
+
+# Adding prebuilt tools
